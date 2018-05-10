9421906d4e2c55bc0f2a25f78f4b4e7e04825a43
Stefan Wagner <stw@bit-strickerei.de>
2018-05-10 12:44:18 +0200

    main/st: change modifier to alt

--- main/st/config.h
+++ main/st/config.h
@@ -162,7 +162,7 @@ static MouseShortcut mshortcuts[] = {
 
 /* Internal keyboard shortcuts. */
 #define MODKEY Mod1Mask
-#define TERMMOD (ControlMask|ShiftMask)
+#define TERMMOD (MODKEY|ShiftMask)
 
 static Shortcut shortcuts[] = {
 	/* mask                 keysym          function        argument */
